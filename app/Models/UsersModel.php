1da09adc7bfa7353e46f46378ea2cf12296b6912
Keerthivasan Sekar <s.keerthivasan@kfinsolutions.com>
2023-04-22 14:01:40 +0530

    user model created and configured

---
+++ app/Models/UsersModel.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsersModel extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'users';
+    protected $primaryKey       = 'user_id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = ['user_name', 'user_email', 'user_password', 'user_fp_token', 'user_deleted'];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_on';
+    protected $updatedField  = 'updated_on';
+    protected $deletedField  = 'deleted_on';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
